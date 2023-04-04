55c2150d659863a44aed5a9ac674215f0d98d99d
feliceclarissa90 <111138811+feliceclarissa90@users.noreply.github.com>
2023-04-04 16:58:28 +0700

    commit

--- src/components/nav/Navigation.js
+++ src/components/nav/Navigation.js
@@ -1,6 +1,6 @@
 import './Navigation.css'
 import profile from "./../../assets/profile.jpeg"
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
 
@@ -9,12 +9,12 @@ const Navigation = () => {
         <div className="sidenav">
             <div className="wrapper">
                 <img src={profile} alt="profile"/>
-                <Link to="/">About</Link>
-                <Link to="/experience">Experience</Link>
-                <Link to="/education">Education</Link>
-                <Link to="/skills">Skills</Link>
-                <Link to="/interests">Interests</Link>
-                <Link to="/awards">Awards</Link>
+                <NavLink to="/">About</NavLink>
+                <NavLink to="/experience">Experience</NavLink>
+                <NavLink to="/education">Education</NavLink>
+                <NavLink to="/skills">Skills</NavLink>
+                <NavLink to="/interests">Interests</NavLink>
+                <NavLink to="/awards">Awards</NavLink>
             </div>
         </div>
     )
